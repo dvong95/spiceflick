db5fb2094d9c8c0076919e79597b2a8adf1108fb
Natalee Bui <buidie@sheridancollege.ca>
2024-07-04 13:39:13 -0400

    Added Message on the NavBar

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,96 +1,143 @@
 "use client";
-import React, { useState } from 'react';
-import {
-  Navbar, Button, NavbarItem, Dropdown, DropdownItem, DropdownTrigger, DropdownMenu, User, Input
-} from "@nextui-org/react";
+import React, { useRef } from 'react';
+import { Navbar, Button, NavbarItem, Dropdown, DropdownItem, DropdownTrigger, DropdownMenu, User, Input, DropdownSection } from "@nextui-org/react";
+import { FiMessageCircle, FiMoreHorizontal } from "react-icons/fi";
+import IUser from '../models/IUser';
+import users from '../data/Users';
+import ThemeSwitcher from './ThemeSwitch';
 
-const NavigationBar = () => {
-  const [albumsOpen, setAlbumsOpen] = useState(false);
+const NavigationBar: React.FC<{ user: IUser }> = ({ user }) => {
 
-  const handleAlbumsToggle = () => {
-    setAlbumsOpen(!albumsOpen);
-  };
 
   return (
-    <Navbar className="flex flex-col items-center py-2 text-foreground w-full">
-      <h1 className="text-3xl font-bold mb-2">SpiceFlick</h1>
+    <>
+      <Navbar className="flex py-2 text-foreground w-full">
+        <h1 className="text-3xl font-bold mb-2">SpiceFlick</h1>
 
-      <div className="mb-4 w-full flex justify-center">
-        <Input
-          isClearable
-          color="primary"
-          placeholder="Search..."
-          className="min-w-[416px] max-w-xs"
-        />
-      </div>
+        {/* Search bar */}
+        <div className="mb-4 w-full flex justify-center">
+          <Input
+            isClearable
+            color="primary"
+            placeholder="Search..."
+            className="min-w-[416px] max-w-xs"
+          />
+        </div>
 
-      <div className="flex space-x-4">
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button variant="flat" className="bg-blue-500 text-white hover:bg-blue-600 px-3 py-1">Demo</Button>
-            </DropdownTrigger>
-            <DropdownMenu>
-              <DropdownItem key="home-default">Home Default</DropdownItem>
-              <DropdownItem key="home-event">Home Event</DropdownItem>
-              <DropdownItem key="landing-page">Landing Page</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
+        {/* Navigation items */}
+        <div className="flex space-x-4">
+          {/* Demo */}
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="flat" className="list-none">Demo</Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem key="home-default">Home Default</DropdownItem>
+                <DropdownItem key="home-event">Home Event</DropdownItem>
+                <DropdownItem key="landing-page">Landing Page</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
 
-        <NavbarItem>
-          <Dropdown closeOnSelect={false}>
-            <DropdownTrigger>
-              <Button variant="flat" className="bg-blue-500 text-white hover:bg-blue-600 px-3 py-1">Pages</Button>
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Static Actions">
-              <DropdownItem>
-                <Dropdown>
-                  <DropdownTrigger>
-                    <Button variant="flat" className="bg-blue-500 text-white hover:bg-blue-600 px-3 py-1">Albums</Button>
-                  </DropdownTrigger>
-                  <DropdownMenu>
-                    <DropdownItem key="album1">Album 1</DropdownItem>
-                    <DropdownItem key="album2">Album 2</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </DropdownItem>
-              <DropdownItem key="blog">Blog</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
+          {/* Pages */}
+          <NavbarItem>
+            <Dropdown closeOnSelect={false}>
+              <DropdownTrigger>
+                <Button variant="flat" className="list-none">Pages</Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem>
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Button variant="flat" color='primary'>Albums</Button>
+                    </DropdownTrigger>
+                    <DropdownMenu>
+                      <DropdownItem key="album1">Album 1</DropdownItem>
+                      <DropdownItem key="album2">Album 2</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </DropdownItem>
+                <DropdownItem key="blog">Blog</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
 
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <User
-                as="button"
-                avatarProps={{
-                  isBordered: true,
-                  src: "https://cdn.mos.cms.futurecdn.net/p5quSf4dZXctG9WFepXFdR-650-80.jpg.webp",
-                }}
-                className="transition-transform"
-                description="@davidvong"
-                name="David Vong"
-              />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="User Actions">
-              <DropdownItem key="profile" className="flex flex-col gap-1">
-                <p className="font-bold text-sm">Signed in as</p>
-                <p className="font-bold text-sm">@davidvong</p>
-              </DropdownItem>
-              <DropdownItem key="settings">My Settings</DropdownItem>
-              <DropdownItem key="privacy_network">Privacy & Network</DropdownItem>
-              <DropdownItem key="analytics">Analytics</DropdownItem>
-              <DropdownItem key="documentation">Documentation</DropdownItem>
-              <DropdownItem key="configurations">Configurations</DropdownItem>
-              <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-              <DropdownItem key="logout" className='text-red-700'>Log Out</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
-      </div>
-    </Navbar>
+          {/* Direct Messages */}
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button variant="flat" className="bg-blue-500 text-white hover:bg-blue-600 p-2">
+                  <FiMessageCircle size={30} />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu className="max-h-48 overflow-y-scroll w-80">
+                <DropdownSection className='sticky top-0 text-default-foreground'>
+                  <DropdownItem key="open-messages">
+                    <Button variant="flat" className="w-full mb-2">Open Messages Tab</Button>
+                  </DropdownItem>
+                </DropdownSection>
+                {users.map((dm, index) => (
+                  <DropdownSection className='max-h-[100px] overflow-hidden'>
+                  <DropdownItem key={`dm${dm.id}`} className="flex items-center justify-between space-x-2 ">
+                    <div className='flex justify-between'>
+                      <div className="flex items-center space-x-2">
+                        <User avatarProps={{ src: dm.profilePicture }} />
+                        <div>
+                          <p className="text-sm font-bold">{dm.name}</p>
+                          <p className="text-sm max-w-[150px] text-gray-500 truncate text-ellipsis">{dm.message}</p>
+                        </div>
+                      </div>
+                      <div>
+                        <FiMoreHorizontal size={20} />
+                      </div>
+                    </div>
+                  </DropdownItem>
+                  </DropdownSection>
+                ))}
+              </DropdownMenu>
+
+            </Dropdown>
+          </NavbarItem>
+
+          {/* User */}
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <User
+                  as="button"
+                  avatarProps={{
+                    isBordered: true,
+                    src: "pictures/po.png",
+                  }}
+                  className="transition-transform"
+                  description="@davidvong"
+                  name="David Vong"
+                />
+              </DropdownTrigger>
+              <DropdownMenu aria-label="User Actions">
+                <DropdownSection aria-label="Profile & Actions" showDivider>
+                  <DropdownItem key="profile" className="flex items-start flex-col gap-1">
+                    <p className="text-xs">Signed in as</p>
+                    <p className="font-bold text-sm">@davidvong</p>
+                  </DropdownItem>
+                </DropdownSection>
+                <DropdownSection showDivider>
+                  <DropdownItem key="settings">My Settings</DropdownItem>
+                  <DropdownItem key="privacy_network">Privacy & Network</DropdownItem>
+                  <DropdownItem key="analytics">Analytics</DropdownItem>
+                  <DropdownItem key="documentation">Documentation</DropdownItem>
+                  <DropdownItem key="configurations">Configurations</DropdownItem>
+                </DropdownSection>
+                <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                <DropdownItem key="logout" className='text-red-700'>Log Out</DropdownItem>
+            
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+        </div>
+      </Navbar>
+    </>
   );
 }
 
--- app/data/Users.tsx
+++ app/data/Users.tsx
@@ -2,29 +2,39 @@ import IUser from "../models/IUser";
 
 let users: IUser[] = [
   {
+    id: 1,
     name: "David Vong",
     profilePicture: "pictures/po.png",
     occupation: "Software Developer",
+    message: "..."
   },
   {
+    id: 2,
     name: "Natalee Tran",
     profilePicture: "pictures/dipsy.png",
     occupation: "A Child",
+     message: "I hate tailwind, it's not working properly"
   },
   {
+    id: 3,
     name: "May Bui",
     profilePicture: "pictures/4may.png",
     occupation: "Older Child",
+     message: "Hi David"
   },
   {
+    id: 4,
     name: "Samuel Verdejo",
     profilePicture: "pictures/tinkywinky.png",
     occupation: "Handsome Developer",
+     message: "I LOVE DAVID!"
   },
   {
+    id: 5,
     name: "Andrew Tadros",
     profilePicture: "pictures/vacuum.png",
     occupation: "Habibi",
+     message: "Hi David"
   },
 ];
 
